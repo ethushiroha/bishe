eca214bdc702bc73bef568ee495de3a2c397db87
ethushiroha <919368842@qq.com>
2022-04-27 11:01:30 +0800

    update pixel statistics

--- code/encode/analyze.py
+++ code/encode/analyze.py
@@ -1,5 +1,32 @@
+import cv2
+import matplotlib.pyplot as plt
+
+
+def plot(x_line: list, y_line: list):
+    plt.bar(x_line, y_line)
+    plt.show()
+
+
+class Img_statistics:
+    def __init__(self, name):
+        self.img = cv2.imread(name, cv2.IMREAD_GRAYSCALE)
+
+    def counts_pixel(self):
+        pixel_list = [0 for i in range(256)]
+        for row in self.img:
+            for col in row:
+                pixel_list[col] += 1
+        return pixel_list
+
+    def draw(self):
+        x_list = [i for i in range(256)]
+        y_list = self.counts_pixel()
+        plot(x_list, y_list)
+
+
 def main():
-    pass
+    img = Img_statistics("/Users/admin/Documents/bishe/code/encode/img.png")
+    img.draw()
 
 
 if __name__ == '__main__':
